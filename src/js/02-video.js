68e7e02ae377b9067dd3700ebebf214c0f61d5d4
NezDvora <122118918+NezDvora@users.noreply.github.com>
2023-05-27 23:53:52 +0300

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', handleTimeUpdate);
+
+document.addEventListener('DOMContentLoaded', restorePlaybackTime);
+
+player.on('timeupdate', throttle(handleTimeUpdate, 1000));
+
+function handleTimeUpdate(event) {
+const currentTime = event.seconds;
+localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+function restorePlaybackTime() {
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+player.setCurrentTime(parseFloat(currentTime));
+}
+}
