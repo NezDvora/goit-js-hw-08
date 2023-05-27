e951394c24bbc6858e8a08a464fb66712e0d6e80
NezDvora <122118918+NezDvora@users.noreply.github.com>
2023-05-27 22:40:00 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
-
+import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('ul');
+
+gallery.style.listStyle= 'none';
+
+const addItems = galleryItems.map(
+(item) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+    </a>
+    </li>`
+).join('');
+
+gallery.insertAdjacentHTML('afterbegin', addItems);
